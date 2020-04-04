733b4af98481dc387ac686d704edccbba2bf654e
dddd <dima0zdoks@gmail.com>
2020-04-04 14:48:46 +0300

    Updates

---
+++ precache-manifest.ceeeefdcfcd0e919e7058053395334f8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff268658e67096bd6da3161926cf3b0",
+    "url": "/test-react-social-network/index.html"
+  },
+  {
+    "revision": "82932690aed36cfafe6e",
+    "url": "/test-react-social-network/static/css/3.89226064.chunk.css"
+  },
+  {
+    "revision": "145483cb9769c6a5ef02",
+    "url": "/test-react-social-network/static/css/4.619b405a.chunk.css"
+  },
+  {
+    "revision": "465b8d74d5c14fbbe4c1",
+    "url": "/test-react-social-network/static/css/main.0451d994.chunk.css"
+  },
+  {
+    "revision": "542c39f17e9ce76b917f",
+    "url": "/test-react-social-network/static/js/2.e0fdf753.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-react-social-network/static/js/2.e0fdf753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82932690aed36cfafe6e",
+    "url": "/test-react-social-network/static/js/3.7f39a845.chunk.js"
+  },
+  {
+    "revision": "145483cb9769c6a5ef02",
+    "url": "/test-react-social-network/static/js/4.c32901fe.chunk.js"
+  },
+  {
+    "revision": "465b8d74d5c14fbbe4c1",
+    "url": "/test-react-social-network/static/js/main.ad9e17e9.chunk.js"
+  },
+  {
+    "revision": "371c9f1737786ced1a09",
+    "url": "/test-react-social-network/static/js/runtime-main.2cf579db.js"
+  },
+  {
+    "revision": "b2d3993760207689b3ac8c79a11ce249",
+    "url": "/test-react-social-network/static/media/preloader.b2d39937.gif"
+  }
+]);
